dd6f804b7a682560e4ccbdeac3b3f501836693df
kabii <jackyd.1018@live.com>
2020-01-02 03:42:16 -0600

    Add check for "skipIfAbsent" option
    Guild configuration option allowing songs to be skipped if user has left channel

--- src/main/kotlin/moe/kabii/discord/audio/AudioEventHandler.kt
+++ src/main/kotlin/moe/kabii/discord/audio/AudioEventHandler.kt
@@ -7,17 +7,46 @@ import com.sedmelluq.discord.lavaplayer.tools.FriendlyException
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason
 import discord4j.core.`object`.entity.TextChannel
+import moe.kabii.data.mongodb.GuildConfigurations
 import moe.kabii.discord.command.commands.audio.AudioCommandContainer
 import moe.kabii.discord.command.errorColor
 import moe.kabii.discord.command.kizunaColor
+import moe.kabii.discord.util.BotUtil
+import moe.kabii.structure.snowflake
+import moe.kabii.structure.tryBlock
 import moe.kabii.util.YoutubeUtil
 
 object AudioEventHandler : AudioEventAdapter() {
     override fun onTrackStart(player: AudioPlayer, track: AudioTrack) {
         val data = track.userData as QueueData
-        // post message when song starts playing if it wasn't direct played
-        data.discord.getChannelById(data.originChannel)
+        val originChan = data.discord.getChannelById(data.originChannel)
             .ofType(TextChannel::class.java)
+
+        val guildID = data.audio.guild
+        val config = GuildConfigurations.getOrCreateGuild(guildID).musicBot
+        // guild option to skip song if queuer has left voice channel.
+        if(config.skipIfAbsent) {
+            val chan = data.discord.getGuildById(guildID.snowflake)
+                .flatMap(BotUtil::getBotVoiceChannel)
+                .tryBlock().orNull()
+            if(chan != null) {
+                val userPresent = chan.voiceStates.filter { state -> state.userId == data.author }.hasElements().tryBlock().orNull()
+                if(userPresent == false) {  // abandon this if it errors, but the bot should definitely be in a voice channel if this is reached
+                    originChan.flatMap { chan ->
+                        chan.createEmbed { embed ->
+                            val title = AudioCommandContainer.trackString(track, includeAuthor = false)
+                            val author = data.author_name
+                            kizunaColor(embed)
+                            embed.setDescription("Skipping **$title** because **$author** left the channel.")
+                        }
+                    }.subscribe()
+                    track.stop()
+                    return
+                }
+            }
+        }
+        // post message when song starts playing if it wasn't direct played
+        originChan
             .flatMap { chan ->
                 val paused = if(player.isPaused) "The bot is currently paused." else ""
                 chan.createEmbed { embed ->
