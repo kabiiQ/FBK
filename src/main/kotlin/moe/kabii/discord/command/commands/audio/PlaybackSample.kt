04efc15734dc8691c5104d23bab2a94b427900e1
kabii <jackyd.1018@live.com>
2020-02-01 09:07:43 -0600

    Add "sampleto" command

--- src/main/kotlin/moe/kabii/discord/command/commands/audio/PlaybackSample.kt
+++ src/main/kotlin/moe/kabii/discord/command/commands/audio/PlaybackSample.kt
@@ -1,5 +1,6 @@
 package moe.kabii.discord.command.commands.audio
 
+import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactive.awaitSingle
 import moe.kabii.discord.audio.AudioManager
 import moe.kabii.discord.audio.QueueData
@@ -24,7 +25,7 @@ object PlaybackSample : AudioCommandContainer {
                 }
                 val sampleTime = DurationParser.tryParse(noCmd)?.run { Try(::toMillis) }?.result?.orNull()
                 if(sampleTime == null) {
-                    error("**$noCmd** is not a valid timestamp. Example: **sample 2m**.").awaitSingle()
+                    error("**$noCmd** is not a valid length of time. Example: **sample 2m**.").awaitSingle()
                     return@discord
                 }
                 val remaining = track.duration - track.position
@@ -38,7 +39,43 @@ object PlaybackSample : AudioCommandContainer {
                 data.endMarkerMillis = endTarget
                 val formattedTime = DurationFormatter(sampleTime).colonTime
                 val skipTime = DurationFormatter(endTarget).colonTime
-                embed("Sampling $formattedTime of ${trackString(track, includeAuthor = false)} -> skipping track at $skipTime").awaitSingle()
+                embed("Sampling $formattedTime of ${trackString(track, includeAuthor = false)} -> skipping track at $skipTime.").awaitSingle()
+            }
+        }
+    }
+
+    object SampleTrackTimestamp : Command("sampleto", "playuntil", "limitto") {
+        init {
+            discord {
+                val audio = AudioManager.getGuildAudio(target.id.asLong())
+                val track = audio.player.playingTrack
+                if(track == null) {
+                    error("There is no track currently playing.").awaitSingle()
+                    return@discord
+                }
+                if(!canFSkip(this, track)) {
+                    error("You must have permission to skip this track before you may limit its playback.").awaitSingle()
+                    return@discord
+                }
+                val sampleTo = DurationParser.tryParse(noCmd)?.run { Try(::toMillis) }?.result?.orNull()
+                if(sampleTo == null) {
+                    error("**$noCmd** is not a valid timestamp. Example: **sample 2m**.").awaitSingle()
+                    return@discord
+                }
+                if(track.position > sampleTo) {
+                    val targetColon = DurationFormatter(sampleTo).colonTime
+                    error("The current track is already beyond the timestamp **$targetColon**.").awaitSingle()
+                    return@discord
+                }
+                if(sampleTo > track.duration) {
+                    val duration = DurationFormatter(track.duration).colonTime
+                    error("The current track does not contain $duration of audio.").awaitFirst()
+                    return@discord
+                }
+                val data = track.userData as QueueData
+                data.endMarkerMillis = sampleTo
+                val skipAt = DurationFormatter(sampleTo).colonTime
+                embed("Sampling ${trackString(track, includeAuthor = false)} -> skipping track at $skipAt.").awaitSingle()
             }
         }
     }
