c358c3f7a5fd826a462a9a0a91ec209c3833b96c
kabii <jackyd.1018@live.com>
2020-02-15 18:15:28 -0600

    Add ability to get current prefix/suffix

--- src/main/kotlin/moe/kabii/discord/command/commands/configuration/CommandOptions.kt
+++ src/main/kotlin/moe/kabii/discord/command/commands/configuration/CommandOptions.kt
@@ -10,11 +10,13 @@ object CommandOptions : CommandContainer {
     object Prefix : Command("prefix", "setprefix", "prefix-set", "set-prefix", "changeprefix") {
         init {
             discord {
-                member.verify(Permission.MANAGE_GUILD)
                 if(args.isEmpty()) {
-                    usage("Sets the command prefix for **${target.name}**.", "prefix !").awaitSingle()
+                    // display current prefix
+                    val prefix = config.prefix
+                    embed("The current command prefix for **${target.name}** is **$prefix**. Command example for changing prefix: **prefix $prefix**.").awaitSingle()
                     return@discord
                 }
+                member.verify(Permission.MANAGE_GUILD)
                 config.prefix = args[0]
                 config.save()
                 embed("Command prefix for **${target.name}** has been set to **${args[0]}** Commands are also accessible using the global bot prefix (;;)").awaitSingle()
@@ -25,11 +27,12 @@ object CommandOptions : CommandContainer {
     object Suffix : Command("suffix", "setsuffix", "set-suffix", "changesuffix") {
         init {
             discord {
-                member.verify(Permission.MANAGE_GUILD)
                 if(args.isEmpty()) {
-                    usage("Sets the command suffix for **${target.name}**. The suffix can be removed with **suffix none**.", "suffix desu").awaitSingle()
+                    val suffix = config.suffix
+                    embed("The current command prefix for **${target.name}** is **$suffix**. Command example for changing suffix: **suffix desu**. The suffix can be removed with **suffix remove**.").awaitSingle()
                     return@discord
                 }
+                member.verify(Permission.MANAGE_GUILD)
                 val suffix = when(args[0]) {
                     "none", "remove", "reset" -> null
                     else -> args[0]
