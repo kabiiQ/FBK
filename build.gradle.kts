f1f25f82bdfcd5ab2df2852e5544093a15f008c6
kabii <jackyd.1018@live.com>
2020-04-07 15:52:24 -0500

    Update to Kotlin 1.3.71

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 group = "moe.kabii"
 
 plugins {
-    val KOTLIN_VER = "1.3.61"
+    val KOTLIN_VER = "1.3.71"
     kotlin("jvm") version KOTLIN_VER
     kotlin("kapt") version KOTLIN_VER
     id("com.github.johnrengelman.shadow") version "5.2.0"
