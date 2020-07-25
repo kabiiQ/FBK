c21cbec69ae50bcc091b1f61ce6d3b1aa62607c1
kabii <jackyd.1018@live.com>
2020-07-25 11:02:10 -0500

    Refactor embed color methods

--- src/main/kotlin/moe/kabii/command/Command.kt
+++ src/main/kotlin/moe/kabii/command/Command.kt
@@ -1,16 +1,10 @@
 package moe.kabii.command
 
-import discord4j.core.`object`.entity.Member
-import discord4j.core.`object`.entity.Role
-import discord4j.core.spec.EmbedCreateSpec
-import discord4j.rest.util.Color
 import discord4j.rest.util.Permission
 import moe.kabii.command.params.DiscordParameters
 import moe.kabii.command.params.TerminalParameters
 import moe.kabii.command.params.TwitchParameters
-import moe.kabii.discord.util.RoleUtil
 import moe.kabii.structure.SourcePaths
-import reactor.core.publisher.Mono
 
 // Now purely aesthetic, Command inheritance is reflectively searched
 interface CommandContainer
@@ -51,17 +45,6 @@ abstract class Command(val baseName: String, vararg alias: String) {
         executeTerminal = block
     }
 }
-
-fun errorColor(spec: EmbedCreateSpec) = spec.setColor(Color.RED)
-fun fbkColor(spec: EmbedCreateSpec) = spec.setColor(Color.of(12187102))
-fun specColor(spec: EmbedCreateSpec) = spec.setColor(Color.of(13369088))
-fun reminderColor(spec: EmbedCreateSpec) = spec.setColor(Color.of(44031))
-fun logColor(member: Member?, spec: EmbedCreateSpec) =
-    Mono.justOrEmpty(member)
-        .flatMap { m -> RoleUtil.getColorRole(m!!) } // weird type interaction means this is Member? but it will never be null inside the operators
-        .map(Role::getColor)
-        .map(spec::setColor)
-        .defaultIfEmpty(fbkColor(spec))
 
 class GuildTargetInvalidException(val string: String) : RuntimeException()
 class FeatureDisabledException(val feature: String, val origin: DiscordParameters) : RuntimeException()
