6defe44e44ec829c57e3e5b94e121bfb81d59be4
kabii <jackyd.1018@live.com>
2020-09-22 02:32:33 -0500

    Remove old 'games' package

--- src/main/kotlin/moe/kabii/discord/games/base/GameManager.kt
+++
@@ -1,9 +0,0 @@
-package moe.kabii.discord.games.base
-
-import discord4j.core.GatewayDiscordClient
-
-enum class GameType(val clazz: Class<out GameBase<*>>)
-
-class GameManager(val discord: GatewayDiscordClient) {
-    val currentGames = mutableListOf<GameBase<*>>()
-}
