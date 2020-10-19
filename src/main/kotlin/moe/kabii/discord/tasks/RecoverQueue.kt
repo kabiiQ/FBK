a573373b3e482e4613dc28e9b37503dbd0650bab
kabii <jackyd.1018@live.com>
2020-10-19 03:33:48 -0500

    Add delay before invoking RecoverQueue

--- src/main/kotlin/moe/kabii/discord/tasks/RecoverQueue.kt
+++ src/main/kotlin/moe/kabii/discord/tasks/RecoverQueue.kt
@@ -7,10 +7,7 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrack
 import discord4j.core.`object`.entity.Guild
 import discord4j.core.`object`.entity.channel.TextChannel
 import discord4j.core.`object`.entity.channel.VoiceChannel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlinx.coroutines.reactive.awaitSingle
 import kotlinx.coroutines.reactor.mono
 import moe.kabii.data.mongodb.GuildConfigurations
@@ -44,40 +41,45 @@ object RecoverQueue {
                 }
 
                 // deserialize and try to re-queue all tracks in order
-                var fail = 0
-                audio.editQueue {
-                    activeQueue.take(20).forEach { track -> // arbitrary limit of 20 for optimization may change
-                        AudioManager.manager.loadItem(track.uri, object : AudioLoadResultHandler {
-                            private fun failed() { fail++ } // count tracks that we can not recover, tracks may no longer be accessible or playable. this is to be expected
-                            override fun noMatches() = failed()
-                            override fun loadFailed(exception: FriendlyException) = failed()
-                            override fun playlistLoaded(playlist: AudioPlaylist) = failed() // db stores tracks, should not encounter a playlist unless the source was changed
-                            override fun trackLoaded(loaded: AudioTrack) {
-                                loaded.userData = QueueData(
-                                    audio,
-                                    discord = guild.client,
-                                    author_name = track.author_name,
-                                    author = track.author.snowflake,
-                                    originChannel = track.originChannel.snowflake,
-                                    volume = config.musicBot.startingVolume
-                                )
-                                add(loaded)
-                            }
-                        }).get()
-                    }
-                    if(audio.queue.isNotEmpty()) {
-                        val size = audio.queue.size
-                        val first = removeAt(0)
-                        audio.player.playTrack(first)
-                        val data = first.userData as QueueData
-                        guild.getChannelById(data.originChannel)
-                            .ofType(TextChannel::class.java)
-                            .flatMap { chan ->
-                                chan.createEmbed { spec ->
-                                    fbkColor(spec)
-                                    spec.setDescription("Recovering from restart: $size tracks loaded, $fail tracks lost.")
+                if(activeQueue.isNotEmpty()) {
+                    delay(10000L)
+                    println("Running RecoverQueue task: ${guild.id.asString()}")
+
+                    var fail = 0
+                    audio.editQueue {
+                        activeQueue.take(20).forEach { track -> // arbitrary limit of 20 for optimization may change
+                            AudioManager.manager.loadItem(track.uri, object : AudioLoadResultHandler {
+                                private fun failed() { fail++ } // count tracks that we can not recover, tracks may no longer be accessible or playable. this is to be expected
+                                override fun noMatches() = failed()
+                                override fun loadFailed(exception: FriendlyException) = failed()
+                                override fun playlistLoaded(playlist: AudioPlaylist) = failed() // db stores tracks, should not encounter a playlist unless the source was changed
+                                override fun trackLoaded(loaded: AudioTrack) {
+                                    loaded.userData = QueueData(
+                                        audio,
+                                        discord = guild.client,
+                                        author_name = track.author_name,
+                                        author = track.author.snowflake,
+                                        originChannel = track.originChannel.snowflake,
+                                        volume = config.musicBot.startingVolume
+                                    )
+                                    add(loaded)
                                 }
-                            }.awaitSingle()
+                            }).get()
+                        }
+                        if(audio.queue.isNotEmpty()) {
+                            val size = audio.queue.size
+                            val first = removeAt(0)
+                            audio.player.playTrack(first)
+                            val data = first.userData as QueueData
+                            guild.getChannelById(data.originChannel)
+                                .ofType(TextChannel::class.java)
+                                .flatMap { chan ->
+                                    chan.createEmbed { spec ->
+                                        fbkColor(spec)
+                                        spec.setDescription("Recovering from restart: $size tracks loaded, $fail tracks lost.")
+                                    }
+                                }.awaitSingle()
+                        }
                     }
                 }
             }
