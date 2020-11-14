f744d74dbab5e8cdb0e56739eadb01d70fe206a1
kabii <jackyd.1018@live.com>
2020-11-14 03:10:51 -0600

    add "tracked" command to list tracked targets

---
+++ src/main/kotlin/moe/kabii/command/commands/trackers/ListTracked.kt
@@ -0,0 +1,90 @@
+package moe.kabii.command.commands.trackers
+
+import discord4j.core.spec.EmbedCreateSpec
+import discord4j.rest.util.Permission
+import kotlinx.coroutines.reactive.awaitSingle
+import moe.kabii.command.Command
+import moe.kabii.data.relational.anime.TrackedMediaLists
+import moe.kabii.data.relational.discord.DiscordObjects
+import moe.kabii.data.relational.streams.TrackedStreams
+import moe.kabii.discord.conversation.Page
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+
+object ListTracked : Command("tracked", "listtracked", "whotracked") {
+    override val wikiPath = "Livestream-Tracker#listing-tracked-streams-with-tracked"
+
+    init {
+        discord {
+            // ;tracked
+            // MAL/$id: by @user
+            if(guild != null) {
+                channelVerify(Permission.MANAGE_MESSAGES)
+            }
+
+            // compile all targets for this channel
+            val tracks = mutableListOf<String>()
+
+            newSuspendedTransaction {
+                val dbChannel = DiscordObjects.Channel.find {
+                    DiscordObjects.Channels.channelID eq chan.id.asLong()
+                }.firstOrNull()
+
+                if(dbChannel == null) {
+                    error("There are no trackers enabled in this channel.").awaitSingle()
+                    return@newSuspendedTransaction
+                }
+
+                // get all tracked stream channels in this channel
+                TrackedStreams.Target.find {
+                    TrackedStreams.Targets.discordChannel eq dbChannel.id
+                }.mapTo(tracks) { target ->
+                    val stream = target.streamChannel
+                    "${stream.site.targetType.full}/${stream.siteChannelID}: by <@${target.tracker.userID}>"
+                }
+
+                // get all tracked anime lists in this channel
+                TrackedMediaLists.ListTarget.find {
+                    TrackedMediaLists.ListTargets.discord eq dbChannel.id
+                }.mapTo(tracks) { target ->
+                    val list = target.mediaList
+                    "${list.site.targetType.full}/${list.siteListId}: by <@${target.userTracked.userID}>"
+                }
+            }
+
+            if(tracks.isEmpty()) {
+                error("There are no tracked targets in this channel.").awaitSingle()
+                return@discord
+            }
+
+            // display up to 20 of these track targets at a time
+            // discord character limit max would put us at about 34, choosing to display less
+            val trackPages = tracks.chunked(32)
+
+            var page: Page? = Page(trackPages.size, 0)
+            var first = true
+
+            // embed spec consumer sets content to current 'page'
+            fun applyPageContent(spec: EmbedCreateSpec) {
+                val currentPage = page!!
+                spec.setTitle("Tracked targets in <#${chan.id.asString()}")
+                val pageContent = trackPages[currentPage.current]
+                spec.setDescription(pageContent.joinToString("\n"))
+                spec.setFooter("Target page ${currentPage.current + 1}/${currentPage.pageCount}", null)
+            }
+
+            val message = embed(::applyPageContent).awaitSingle()
+
+            if(page!!.pageCount > 1) {
+                while (page != null) {
+                    if (!first) {
+                        message.edit { spec ->
+                            spec.setEmbed(::applyPageContent)
+                        }.awaitSingle()
+                    }
+                    page = getPage(page, message, add = first)
+                    first = false
+                }
+            }
+        }
+    }
+}
