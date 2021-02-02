f7acec8a2f6ff1974edd0a141251d6533e38a4e5
kabii <jackyd.1018@live.com>
2021-02-01 20:21:11 -0600

    Reaction-role behavior change
    no longer straight "toggle" roles - use reaction add/remove to perform respective role add/remove

--- src/main/kotlin/moe/kabii/discord/event/guild/ReactionRoleHandler.kt
+++ src/main/kotlin/moe/kabii/discord/event/guild/ReactionRoleHandler.kt
@@ -15,34 +15,28 @@ import moe.kabii.structure.extensions.orNull
 import moe.kabii.structure.extensions.snowflake
 import moe.kabii.structure.extensions.tryAwait
 import reactor.core.publisher.Mono
+import java.time.Duration
+
+enum class ReactionAction { ADD, REMOVE }
 
 object ReactionRoleHandler {
     object ReactionAddListener : EventListener<ReactionAddEvent>(ReactionAddEvent::class) {
         override suspend fun handle(event: ReactionAddEvent) {
             val guildId = event.guildId.orNull() ?: return
-            val config = GuildConfigurations.getOrCreateGuild(guildId.asLong())
-            val clean = config.options.featureChannels[event.channelId.asLong()]?.cleanReactionRoles
-
-            handleReactionRole(event.messageId, event.guild, event.message, config, event.userId, event.emoji, clean)
+            handleReactionRole(event.userId, guildId, event.channelId, event.messageId, event.guild, event.message, event.emoji, ReactionAction.ADD)
         }
     }
 
     object ReactionRemoveListener : EventListener<ReactionRemoveEvent>(ReactionRemoveEvent::class) {
         override suspend fun handle(event: ReactionRemoveEvent) {
             val guildId = event.guildId.orNull() ?: return
-            val config = GuildConfigurations.getOrCreateGuild(guildId.asLong())
-
-            // if this channel is set up with "clean" reaction roles, ignore reaction "remove" events. if this feature is not enabled, they are handled the same as "add" events.
-            val clean = config.options.featureChannels[event.channelId.asLong()]?.cleanReactionRoles
-            if(clean == true) return
-
-            handleReactionRole(event.messageId, event.guild, event.message, config, event.userId, event.emoji, clean)
+            handleReactionRole(event.userId, guildId, event.channelId, event.messageId, event.guild, event.message, event.emoji, ReactionAction.REMOVE)
         }
     }
 
-    // handle add and removes the same - they are a toggle depending on the user having the role, not
-    suspend fun handleReactionRole(messageId: Snowflake, guild: Mono<Guild>, message: Mono<Message>, config: GuildConfiguration, userId: Snowflake, emoji: ReactionEmoji, clean: Boolean?) {
+    suspend fun handleReactionRole(userId: Snowflake, guildId: Snowflake, channelId: Snowflake, messageId: Snowflake, guild: Mono<Guild>, message: Mono<Message>, emoji: ReactionEmoji, direction: ReactionAction) {
         // check if this is a registered reaction role message
+        val config = GuildConfigurations.getOrCreateGuild(guildId.asLong())
         val reactionRole = config.selfRoles.reactionRoles
             .filter { cfg -> cfg.message.messageID == messageId.asLong() }
             .find { cfg -> cfg.reaction.toReactionEmoji() == emoji }
@@ -51,21 +45,23 @@ object ReactionRoleHandler {
         val info = "Reaction role message #${reactionRole.message.messageID} / emoji ${reactionRole.reaction.name}"
         val member = guild
             .flatMap { g -> g.getMemberById(userId) }.awaitSingle()!!
-
         if(member.isBot) return
 
         val reactionRoleId = reactionRole.role.snowflake
-        val hasRole = member.roleIds.contains(reactionRoleId)
-
-        val action = if(hasRole) member.removeRole(reactionRole.role.snowflake, info)
-        else member.addRole(reactionRole.role.snowflake, info)
+        val action = when(direction) {
+            ReactionAction.ADD -> member.addRole(reactionRoleId, info)
+            ReactionAction.REMOVE -> member.removeRole(reactionRoleId, info)
+        }
 
         try {
             action.thenReturn(Unit).awaitSingle()
 
             // if "clean" reaction-role config is enabled, remove user reaction here
-            if(clean == true) {
-                message.flatMap { m -> m.removeReaction(emoji, userId) }
+            val clean = config.options.featureChannels[channelId.asLong()]?.cleanReactionRoles
+            if(direction == ReactionAction.ADD && clean == true) {
+                message
+                    .delayElement(Duration.ofSeconds(30))
+                    .flatMap { m -> m.removeReaction(emoji, userId) }
                     .thenReturn(Unit)
                     .tryAwait()
             }
