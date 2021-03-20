2b1bd758e447e7efcb3264c3f2b89d5405636a92
kabii <jackyd.1018@live.com>
2021-03-19 20:05:43 -0500

    fix long standing error throwing by deletelogs

--- src/main/kotlin/moe/kabii/discord/event/message/MessageDeletionListener.kt
+++ src/main/kotlin/moe/kabii/discord/event/message/MessageDeletionListener.kt
@@ -32,19 +32,27 @@ object MessageDeletionListener : EventListener<MessageDeleteEvent>(MessageDelete
 
         // only can really handle delete log if the message is currently logged. discord does not provide much information here.
         // attempt to recover message information
+        var authorId: Long? = null
+        var content: String? = null
         val sessionMessage = event.message.orNull()
-        val (authorID, content) = if(sessionMessage != null) {
+        if(sessionMessage != null) {
             if(sessionMessage.author.map(User::isBot).orNull() != false) return
-            sessionMessage.author.get().id.asLong() to sessionMessage.content
+            authorId = sessionMessage.author.get().id.asLong()
+            content = sessionMessage.content
         }
-        else transaction { history?.author?.userID to history?.content }
+        else {
+            transaction {
+                authorId = history?.author?.userID
+                content = history?.content
+            }
+        }
 
         if(history != null) transaction {
             history.delete() // remove this message from the database if logged
         }
 
         // fall back to null if we can't get the author either
-        val author = if(authorID != null) event.client.getUserById(authorID.snowflake).tryAwait().orNull() else null
+        val author = if(authorId != null) event.client.getUserById(authorId!!.snowflake).tryAwait().orNull() else null
         val channelName = event.channel.ofType(GuildChannel::class.java).awaitSingle().name
         val embedAuthor = if(author != null) {
             "A message by ${author.userAddress()} was deleted in #$channelName:"
@@ -59,7 +67,7 @@ object MessageDeletionListener : EventListener<MessageDeleteEvent>(MessageDelete
                         logChan.createEmbed { spec ->
                             fbkColor(spec)
                             spec.setAuthor(embedAuthor, null, author?.avatarUrl)
-                            val deletedContent = if(content.isNotEmpty()) "Deleted message: $content" else "The deleted message had no content. (file/embed)"
+                            val deletedContent = if(content.isNullOrBlank()) "Deleted message: $content" else "The deleted message had no message. (file/embed)"
                             spec.setDescription(deletedContent)
                             spec.setFooter("Deleted Message ID: ${event.messageId.asString()} - Original message timestamp", null)
                             spec.setTimestamp(event.messageId.timestamp)
