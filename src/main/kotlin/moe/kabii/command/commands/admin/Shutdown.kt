7a21e55e5ebce204cd1d9bb37263acc7bf9dfabf
kabii <jackyd.1018@live.com>
2021-08-03 13:36:20 -0500

    add force/soft shutdown command

--- src/main/kotlin/moe/kabii/command/commands/admin/Shutdown.kt
+++ src/main/kotlin/moe/kabii/command/commands/admin/Shutdown.kt
@@ -1,16 +1,37 @@
 package moe.kabii.command.commands.admin
 
 import moe.kabii.command.Command
+import moe.kabii.discord.audio.AudioManager
+import moe.kabii.discord.audio.GuildAudio
 import moe.kabii.discord.conversation.Conversation
 import kotlin.system.exitProcess
 
-object Shutdown : Command("shutdown", "end") {
+object Shutdown : Command("stop", "shutdown", "end") {
     override val wikiPath: String? = null
 
     init {
         terminal {
             println("Ending process...")
+            val conversations = Conversation.conversations.size
+            val activeAudio = AudioManager.guilds.values.count(GuildAudio::playing)
+            if(conversations > 0 || activeAudio > 0) {
+                println("There are $conversations active conversations and $activeAudio guilds have audio playing. Use forcestop to end.")
+            } else {
+                println("No active conversations. Exiting!")
+                exitProcess(0)
+            }
+        }
+    }
+}
+
+object ForceShutdown : Command("forcestop", "force", "forceshutdown") {
+    override val wikiPath: String? = null
+
+    init {
+        terminal {
+            println("Force-ending...")
             val conversations = Conversation.shutdown()
+            // todo warn users audio players are being ended
             println("Ended $conversations active conversations.")
             exitProcess(0)
         }
