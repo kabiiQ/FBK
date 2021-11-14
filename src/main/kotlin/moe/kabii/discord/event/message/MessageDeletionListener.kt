d079a2612d59449a7ff91679e57b8dddf153bfea
kabii <jackyd.1018@live.com>
2021-11-13 22:13:16 -0600

    patch for duplicate yt upload notifs

--- src/main/kotlin/moe/kabii/discord/event/message/MessageDeletionListener.kt
+++ src/main/kotlin/moe/kabii/discord/event/message/MessageDeletionListener.kt
@@ -9,8 +9,6 @@ import kotlinx.coroutines.reactive.awaitSingle
 import moe.kabii.LOG
 import moe.kabii.data.mongodb.GuildConfigurations
 import moe.kabii.data.relational.discord.MessageHistory
-import moe.kabii.discord.auditlog.AuditableEvent
-import moe.kabii.discord.auditlog.LogWatcher
 import moe.kabii.discord.event.EventListener
 import moe.kabii.discord.util.fbkColor
 import moe.kabii.util.extensions.*
