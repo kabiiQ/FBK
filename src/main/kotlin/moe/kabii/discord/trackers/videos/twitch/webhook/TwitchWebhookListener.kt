81d19155754c997067e37bc5ef58535da50defe9
kabii <jackyd.1018@live.com>
2022-02-19 12:08:34 -0600

    add slight delay to twitch events to ensure info is available

--- src/main/kotlin/moe/kabii/discord/trackers/videos/twitch/webhook/TwitchWebhookListener.kt
+++ src/main/kotlin/moe/kabii/discord/trackers/videos/twitch/webhook/TwitchWebhookListener.kt
@@ -7,12 +7,18 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.time.delay
 import moe.kabii.LOG
 import moe.kabii.MOSHI
 import moe.kabii.data.flat.Keys
 import moe.kabii.data.relational.streams.TrackedStreams
 import moe.kabii.data.relational.streams.twitch.TwitchEventSubscription
 import moe.kabii.data.relational.streams.twitch.TwitchEventSubscriptions
+import moe.kabii.discord.tasks.DiscordTaskPool
 import moe.kabii.discord.trackers.videos.twitch.json.TwitchEvents
 import moe.kabii.discord.trackers.videos.twitch.parser.TwitchParser
 import moe.kabii.discord.trackers.videos.twitch.watcher.TwitchChecker
@@ -21,12 +27,15 @@ import moe.kabii.util.extensions.propagateTransaction
 import moe.kabii.util.extensions.stackTraceString
 import org.apache.commons.codec.digest.HmacAlgorithms
 import org.apache.commons.codec.digest.HmacUtils
+import java.time.Duration
 
 class TwitchWebhookListener(val manager: TwitchSubscriptionManager, val checker: TwitchChecker) {
 
     private val hmac256 = HmacUtils(HmacAlgorithms.HMAC_SHA_256, Keys.config[Keys.Twitch.signingKey])
     private val eventAdapter = MOSHI.adapter(TwitchEvents.Response.EventNotification::class.java)
     private val port = Keys.config[Keys.Twitch.listenPort]
+
+    private val intakeContext = CoroutineScope(DiscordTaskPool.twitchIntakeThread + CoroutineName("TwitchWebHook-Notifier") + SupervisorJob())
 
     // w3c websub
     val server = embeddedServer(Netty, port = port) {
@@ -40,8 +49,7 @@ class TwitchWebhookListener(val manager: TwitchSubscriptionManager, val checker:
 
                     val signature = call.request.header("Twitch-Eventsub-Message-Signature")
 
-                    // allows proper utf 8 interpretation
-                    val body = call.receiveStream().bufferedReader().readText()
+                    val body = call.receiveStream().bufferedReader().readText() // allows proper utf 8 interpretation
                     val message = call.request.header("Twitch-Eventsub-Message-Id") +
                             call.request.header("Twitch-Eventsub-Message-Timestamp") +
                             body
@@ -81,19 +89,22 @@ class TwitchWebhookListener(val manager: TwitchSubscriptionManager, val checker:
 
                                 TwitchEventSubscriptions.Type.START_STREAM -> {
 
-                                    val twitchStream = TwitchParser.getStream(notification.userId.toLong()).orNull()
-                                    if(twitchStream == null) {
-                                        LOG.warn("Twitch stream started: ${notification.userLogin} but there was an error retrieving the live stream info!")
-                                        return@post
-                                    }
-                                    propagateTransaction {
-                                        val channel = TrackedStreams.StreamChannel.getChannel(TrackedStreams.DBSite.TWITCH, notification.userId)
-                                        val targets = channel?.run { checker.getActiveTargets(this) }
-                                        if(channel == null || targets == null) {
-                                            LOG.warn("Payload received for untracked channel: $channel + targets: $targets + payload: $body")
-                                            return@propagateTransaction
+                                    intakeContext.launch {
+                                        delay(Duration.ofSeconds(8))
+                                        val twitchStream = TwitchParser.getStream(notification.userId.toLong()).orNull()
+                                        if(twitchStream == null) {
+                                            LOG.warn("Twitch stream started: ${notification.userLogin} but there was an error retrieving the live stream info!")
+                                            return@launch
                                         }
-                                        checker.updateChannel(channel, twitchStream, targets)
+                                        propagateTransaction {
+                                            val channel = TrackedStreams.StreamChannel.getChannel(TrackedStreams.DBSite.TWITCH, notification.userId)
+                                            val targets = channel?.run { checker.getActiveTargets(this) }
+                                            if(channel == null || targets == null) {
+                                                LOG.warn("Payload received for untracked channel: $channel + targets: $targets + payload: $body")
+                                                return@propagateTransaction
+                                            }
+                                            checker.updateChannel(channel, twitchStream, targets)
+                                        }
                                     }
                                 }
                             }
