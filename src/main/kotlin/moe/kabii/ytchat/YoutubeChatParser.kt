81054e881b07e00217b101cb0e3eb688d5fce9c9
kabii <jackyd.1018@live.com>
2023-06-14 20:02:32 -0500

    initial base for replacing youtube emojis in chat messages

--- src/main/kotlin/moe/kabii/ytchat/YoutubeChatParser.kt
+++ src/main/kotlin/moe/kabii/ytchat/YoutubeChatParser.kt
@@ -1,5 +1,6 @@
 package moe.kabii.ytchat
 
+import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Types
 import moe.kabii.LOG
 import moe.kabii.MOSHI
@@ -7,11 +8,35 @@ import moe.kabii.data.relational.streams.youtube.ytchat.YoutubeMember
 import moe.kabii.instances.DiscordInstances
 import moe.kabii.util.extensions.propagateTransaction
 import moe.kabii.util.extensions.stackTraceString
+import java.io.File
 
 class YoutubeChatParser(val instances: DiscordInstances, val watcher: YoutubeChatWatcher) {
 
     private val chatDataType = Types.newParameterizedType(List::class.java, YTChatMessage::class.java)
     private val chatAdapter = MOSHI.adapter<List<YTChatMessage>>(chatDataType)
+
+
+    /*
+    Objects for mapping chat emojis to discord-compatible text.
+     */
+    private val emojiMap = mutableMapOf<String, String>()
+    @JsonClass(generateAdapter = true)
+    data class YoutubeEmojiMapping(val yt: String, val discord: String)
+    val emojiDataType = Types.newParameterizedType(List::class.java, YoutubeEmojiMapping::class.java)
+    private val emojiAdapter = MOSHI.adapter<List<YoutubeEmojiMapping>>(emojiDataType)
+
+    init {
+        // load emoji map from file
+        val map = File("files/youtube/emoji.json")
+        try {
+            emojiAdapter
+                .fromJson(map.readText())!!
+                .associateTo(emojiMap) { m -> m.yt to m.discord }
+        } catch(e: Exception) {
+            LOG.error("Error loading YouTube emoji replacements: ${e.message}")
+            LOG.error(e.stackTraceString)
+        }
+    }
 
     suspend fun handleChatData(data: YoutubeChatWatcher.YTChatData) {
         val (room, json) = data
@@ -28,6 +53,7 @@ class YoutubeChatParser(val instances: DiscordInstances, val watcher: YoutubeCha
             propagateTransaction {
                 messages
                     .filter { chat -> chat.type == "textMessage" }
+                    .map(::convertYoutubeEmoji)
                     .onEach { message -> watcher.holoChatQueue.trySend(YoutubeChatWatcher.YTMessageData(room, message)) }
                     .filter { chat -> chat.author.member }
                     .forEach { chat ->
@@ -38,5 +64,22 @@ class YoutubeChatParser(val instances: DiscordInstances, val watcher: YoutubeCha
             LOG.error("Problem committing YT chat data to db: ${e.message}")
             LOG.warn(e.stackTraceString)
         }
+    }
+
+    private val youtubeEmoji = Regex(":([a-zA-Z]{2,24}):")
+    /**
+     * @param YTChatMessage containing a raw YouTube message.
+     * @return A new YTChatMessage with the message contents changed to replace emojis with Discord-compatible naming
+     */
+    private fun convertYoutubeEmoji(data: YTChatMessage): YTChatMessage {
+        // find possible emojis
+        val matches = youtubeEmoji.findAll(data.message)
+        val converted = matches.fold(data.message) { acc, match ->
+            val replacement = emojiMap[match.groups[1]!!.value]
+            if(replacement != null) {
+                acc.replace(match.value, replacement)
+            } else acc
+        }
+        return data.copy(message = converted)
     }
 }
