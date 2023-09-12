57deda0488cd0af5c0e26d2803de6f2d386d21cb
kabii <jackyd.1018@live.com>
2023-09-12 07:54:50 -0500

    add admin command to untrack x feeds

--- src/main/kotlin/moe/kabii/command/commands/admin/ToggleTwitter.kt
+++ src/main/kotlin/moe/kabii/command/commands/admin/ToggleTwitter.kt
@@ -2,6 +2,11 @@ package moe.kabii.command.commands.admin
 
 import moe.kabii.command.Command
 import moe.kabii.data.TempStates
+import moe.kabii.data.relational.twitter.TwitterFeeds
+import moe.kabii.util.extensions.propagateTransaction
+import org.jetbrains.exposed.sql.LowerCase
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
 
 object ToggleTwitter : Command("toggletwitter") {
     override val wikiPath: String? = null
@@ -15,6 +20,25 @@ object ToggleTwitter : Command("toggletwitter") {
                 println("Disabling Twitter checker")
                 TempStates.skipTwitter = true
             }
+        }
+    }
+}
+
+object UntrackTwitterFeed : Command("untrackfeed") {
+    override val wikiPath: String? = null
+
+    init {
+        terminal {
+            if(args.isEmpty()) {
+                println("Command usage: untrackfeed <feed name>")
+                return@terminal
+            }
+            val deleted = propagateTransaction {
+                TwitterFeeds.deleteWhere {
+                    LowerCase(TwitterFeeds.username) eq args[0].lowercase()
+                }
+            }
+            println("DELETED $deleted")
         }
     }
 }
